c656914fd838b2dfcc49159952ef07add890ab2f
Fernando Heleno <feheleno@gmail.com>
2023-01-15 16:40:23 -0300

    section 3 - video 52 completed

--- react_basics/monsters-rolodex/src/App.js
+++ react_basics/monsters-rolodex/src/App.js
@@ -1,36 +1,55 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      monsters: [
-        {
-          id: '0001',
-          name: 'Linda',
-        },
-        {
-          id: '0002',
-          name: 'Frank',
-        },
-        {
-          id: '0003',
-          name: 'Jacky',
-        },
-      ]  
+      monsters: [],
+      searchField: ''
     };
   }
-  
+
+  //Lyfecycle Method which executes only one time, that is, it executes only when the component mounts
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return { monsters: users };
+      },
+        () => {
+          console.log(this.state);
+        }
+      ));
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    // monsters = [{name: 'Leanne'}, {name: 'Clementine'}]
+    const filteredMonsters = monsters.filter((filteredMonster) => {
+      return filteredMonster.name.toLocaleLowerCase().includes(searchField)
+    });
+    // input = 'Lea' => filteredMonsters = [{name: 'Leanne'}]
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-            return <h1 key={monster.id}>{monster.name}</h1>;
-          }
-        )}
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox className='monster-search-box' placeholder='search monsters' onChangeHandler={onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
