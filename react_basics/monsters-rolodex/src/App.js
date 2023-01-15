0e99d75882ecafbfb06baae060e89bd63fdf4785
Fernando Heleno <feheleno@gmail.com>
2023-01-14 23:54:32 -0300

    section3 - video 35

--- react_basics/monsters-rolodex/src/App.js
+++ react_basics/monsters-rolodex/src/App.js
@@ -1,25 +1,39 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        {
+          id: '0001',
+          name: 'Linda',
+        },
+        {
+          id: '0002',
+          name: 'Frank',
+        },
+        {
+          id: '0003',
+          name: 'Jacky',
+        },
+      ]  
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+            return <h1 key={monster.id}>{monster.name}</h1>;
+          }
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
