ef4984afc72072d6c9c1e68f53e492e75268954a
Fernando Heleno <feheleno@gmail.com>
2023-01-18 18:20:30 -0300

    section 6 - video 96 completed implemented sign-in with redirect

--- project/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ project/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,47 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebse.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+    auth,
+    signInWithGooglePopup,
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth
+} from "../../utils/firebase/firebse.utils";
 
 
 const SignIn = () => {
-    const logGoogleUser = async () =>{
+    
+    useEffect(() => {(
+        // monsters = [{name: 'Leanne'}, {name: 'Clementine'}]
+        async () => {
+        const response = await getRedirectResult(auth);
+        if(response){
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+        })();
+        // input = 'Lea' => filteredMonsters = [{name: 'Leanne'}]
+    
+        return () => {}
+        
+      }, []);
+    
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+    //     console.log(response);
+    // }, []);
+
+    const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
         console.log(response);
-        const {user} = response;
+        const { user } = response;
         const userDocRef = await createUserDocumentFromAuth(user);
-        
     }
 
     return (
         <div>
             <h1>Sign In page</h1>
-            <button onClick={logGoogleUser} >
-                Sign in with Google Popup
-            </button>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
         </div>
     )
 }
