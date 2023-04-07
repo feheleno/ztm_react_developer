d5df31923a542db8059941d55beab738923d73d6
Fernando Heleno <feheleno@gmail.com>
2023-04-07 20:50:48 -0300

    section9 - video 119 - completed

--- project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,7 @@
-// import { useContext } from 'react';
-// import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+import CartItem from '../cart-item/cart-item.component';
 import Button from '../button/button.component';
 
 import './cart-dropdown.styles.scss';
@@ -7,10 +9,13 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     // const {hidden, setHidden} = useContext(CartContext);
+    const { cartItems } = useContext(CartContext);
     return (
         // <div className='cart-dropdown-container' hidden={hidden} onClick={setHidden(!hidden)} >
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
