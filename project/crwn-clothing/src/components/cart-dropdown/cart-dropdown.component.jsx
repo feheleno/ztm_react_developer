d158276b4b7d7bc518b547c8fde95beb79d03895
Fernando Heleno <feheleno@gmail.com>
2023-04-08 12:10:57 -0300

    section9 - video 122 - completed
    Different way to navigate do the checkout page, looks cleaner without adding <Link> around <Button> and actually adding an action to <Button>

--- project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ project/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,32 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../contexts/cart.context';
-import CartItem from '../cart-item/cart-item.component';
-import Button from '../button/button.component';
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
+import Button from "../button/button.component";
 
-import './cart-dropdown.styles.scss';
-
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-    // const {hidden, setHidden} = useContext(CartContext);
-    const { cartItems } = useContext(CartContext);
-    return (
-        // <div className='cart-dropdown-container' hidden={hidden} onClick={setHidden(!hidden)} >
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
-            </div>
-            <Link to={'checkout'}>
-                <Button to='checkout'>GO TO CHECKOUT</Button>
-            </Link>
-        </div>
-    )
-}
+  // const {hidden, setHidden} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
+
+  return (
+    // <div className='cart-dropdown-container' hidden={hidden} onClick={setHidden(!hidden)} >
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
