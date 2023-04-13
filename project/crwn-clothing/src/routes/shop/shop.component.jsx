c783d73926caa8cd2412d269d7b49c9de99ae4c8
Fernando Heleno <feheleno@gmail.com>
2023-04-13 15:08:16 -0300

    section11 - video 136 - completed

--- project/crwn-clothing/src/routes/shop/shop.component.jsx
+++ project/crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../categories-preview/category/category.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
